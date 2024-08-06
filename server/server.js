929ab8211cd71adbb7aae036f54ecd4424436388
Robert Ramos <robert.ramos07281021@gmail.com>
2024-08-06 20:41:20 +0800

    new1

--- server/server.js
+++ server/server.js
@@ -2,20 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const path = require("path");
+
 const recipesRouter = require("./routers/recipesRouter");
-// const { fileURLToPath } = require("url");
+
 app.use(express.json());
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 app.use("/recipes", recipesRouter);
-
-app.use(express.static(path.join(__dirname, "./dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./dist/index.html"));
-});
 
 mongoose
   .connect(process.env.DB_URI, { dbName: `recipes_db` })
